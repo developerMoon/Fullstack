69127f390712ea5da1cab33fec2f663a7c1c5afe
Soyoung Moon <moonsoyoung24@gmail.com>
2018-09-30 11:57:20 +0100

    #41 enabling cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'); //require: access to express library
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
 
 require('./models/User'); //not executed automatically unless require it here
@@ -10,6 +12,16 @@ mongoose.connect(keys.mongoURI);
 
 const app = express(); //generates new express app, could be single app or more
 //this generated app listens incoming request
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000, //30 days in millisecs
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
