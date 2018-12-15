a527661a898b7b9f0a74b73ed97e9e021dba9fcc
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-15 14:46:33 +0000

    #83 header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,11 @@ class Header extends Component {
   renderContent() {
     switch(this.props.auth) {
       case null:
-        return 'Still deciding';
+        return ;
       case false:
-        return 'logged out';
+        return  <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return 'logged in';
+        return <li><a>Logout</a></li>;
     }
   }
 
