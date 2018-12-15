0ce4daadcc613c286d6cdcf054cde2c0495dded2
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-15 14:33:25 +0000

    #82 accessing state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'logged out';
+      default:
+        return 'logged in';
+    }
+  }
+
   render(){
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
@@ -9,14 +21,15 @@ class Header extends Component {
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     )
   }
 }
+function mapStateToProps({ auth }) {
+  return{ auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,12 +2,12 @@ import { FETCH_USER } from '../actions/types';
 
 export default function(state = null, action){
   //determine whether user logged in
-  console.log(action);
   switch (action.type){
     case FETCH_USER:
       console.log(action.payload);
-      return action.payload || false;
+      return action.payload || false; //action.payload or false
+      //so that empty string is not returned
     default:
-      return state; //default state: empty object {}
+      return state; 
   }
 }
