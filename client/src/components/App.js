36628e560866bcb13e86b6ec7a3b27a28bc1f0a2
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-16 13:20:35 +0000

    #86 Landing component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 //import action creators
 import * as actions from '../actions';
 import Header from './Header';
+import Landing from './Landing'
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 
 class App extends Component {
