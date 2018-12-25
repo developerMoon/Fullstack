d71aa808cbe78c0d124a1201de5d962e4db8402a
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-25 09:37:47 +0000

    #94 payments component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+  render() {
+    debugger;
+    
+    return(
+      <StripeCheckout 
+        amount={500} //500cents = 5usd
+        token={token => console.log(token)} //expects callback function from stripe
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
+}
+
+export default Payments;
