0d1519b7905428177bde79c5ddeb966358900ddf
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-29 13:04:20 +0000

    #97 reusing action types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,9 +4,15 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
+    
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe', token); //after user buys credit, reload
+
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
 
 
 
