bddfaeceb1afee3ade5dde41c77730a8f4d22e0a
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-29 12:54:54 +0000

    #96 Payment Fixes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,10 +6,16 @@ class Payments extends Component {
  
     return(
       <StripeCheckout 
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500} //500cents = 5usd
         token={token => console.log(token)} //expects callback function from stripe
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
