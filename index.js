37d8962b5e098eef9f2ca8e7feec852168881c1c
Soyoung Moon <moonsoyoung24@gmail.com>
2018-12-29 13:17:36 +0000

    #99 post request handlers

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
+require('./routes/billingRoutes')(app); //both export functions
 //this PORT const shouldn't changed 
 const PORT = process.env.PORT || 5000;
 //Heroku inject environment variables
