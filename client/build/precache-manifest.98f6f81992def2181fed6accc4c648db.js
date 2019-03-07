57dc4a9006a034e10e4078db155b40edfe81940b
Soyoung Moon <soyoung.moon@milliman.com>
2019-03-07 18:11:51 +0900

    change gitignore

---
+++ client/build/precache-manifest.98f6f81992def2181fed6accc4c648db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9f1ae48249fe0f4cd7f",
+    "url": "/static/js/main.c9f1ae48.chunk.js"
+  },
+  {
+    "revision": "f6d52585039c1467f76d",
+    "url": "/static/js/1.f6d52585.chunk.js"
+  },
+  {
+    "revision": "f6d52585039c1467f76d",
+    "url": "/static/css/1.1e00c8a0.chunk.css"
+  },
+  {
+    "revision": "5e220db5030f202bd4d80efa1d05815c",
+    "url": "/index.html"
+  }
+];
