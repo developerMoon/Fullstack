43add959d3c9666ae886df899c6ff8c1aae5a13d
Soyoung Moon <moonsoyoung24@gmail.com>
2019-03-09 09:39:26 +0000

    #127  Verifying Minimum credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,9 @@
 const requireLogin = require('../middlewares/requireLogin');
-
+const requireCredits = require('../middlewares/requireCredits');
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, (req, res) => {
-    //if a user is logged in -> requireLogin
-
-    //if a user has enough credits
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    //if a user is logged in -> requireLogin (1)
+    //if a user has enough credits -> reqireCredits (2) 
+    //it has to be in the order as you want it to be executed
   });
 };
