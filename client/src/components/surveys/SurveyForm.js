22decdb48baaaa0249ac75df1f40186023c19a17
Soyoung Moon <moonsoyoung24@gmail.com>
2019-03-23 16:29:22 +0100

    #157 Styling the Form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,44 +1,42 @@
 //SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title'},
+  { label: 'Subject Line', name: 'subject'},
+  { label: 'Email Body', name: 'body'},
+  { label: 'Recipient List', name: 'emails'}
+];
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field 
-          label="Survey Title" 
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field  
+          key={name} 
+          component={SurveyField} 
           type="text" 
-          name="title" 
-          component={SurveyField}
+          label={label} 
+          name={name}
         />
-        <Field 
-          label="Subject Line" 
-          type="text" 
-          name="subject" 
-          component={SurveyField}
-        />
-        <Field 
-          label="Email Body" 
-          type="text" 
-          name="body" 
-          component={SurveyField}
-        />
-        <Field 
-          label="Recipient List" 
-          type="text" 
-          name="emails" 
-          component={SurveyField}
-        />
-      </div>
-    );
+      );    
+    });
   }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>  
       </div>
     );
