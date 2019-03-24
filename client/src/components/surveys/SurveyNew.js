62b2208046bb6e2e41aba9a5144871d09159ecf1
Soyoung Moon <moonsoyoung24@gmail.com>
2019-03-24 14:19:52 +0100

    #164 Advancing from surveyform

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,33 @@
 //SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 class SurveyNew extends Component {
+  //component level state
+  //initializing state : constructor
+  /*constructor(props) {
+    super(props);
+
+    this.state = { new: true };
+  }*/
+
+  state = { showFormReview: false }; //equivalent to constructor method
+
+  renderContent() {
+    if (this.state.showFormReview){ //if true
+      return <SurveyFormReview />;
+    }
+    return (
+      <SurveyForm 
+        onSurveySubmit={() => this.setState({ showFormReview: true })} 
+      /> 
+    );
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     );
   }
