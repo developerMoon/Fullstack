706d79e84a3e6c2fc5aa0f84d61012cf8cb46cdc
Soyoung Moon <moonsoyoung24@gmail.com>
2019-03-25 09:15:18 +0100

    #177 Testing Webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,11 @@ module.exports = app => {
     res.send('Thanks for your feedback');
   });
   
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  });
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     //if a user is logged in -> requireLogin (1)
     //if a user has enough credits -> reqireCredits (2) 
